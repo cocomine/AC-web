29a60d2aac8935bf8fe8df789a111236c447369b
MinecraftCoco <roguish.6888467468@gmail.com>
2018-06-09 11:32:34 +0800

    Update skip.js

--- wp-includes/js/skip.js
+++ wp-includes/js/skip.js
@@ -1,38 +1,22 @@
 var downloading = false;
 
-function skip() {
-jQuery(function($){
-    var delayMillis = 5000; //5 second
+function timer skip() {
+    var delayMillis = 5; //5 second
     var timelift = 1000; //1 sec
     
- if(downloading == false){
-    downloading = true;
-    document.getElementById("wait").innerText="The download will start in 5 seconds...";
-    
-    setTimeout(function() {
-        document.getElementById("wait").innerText="The download will start in 4 seconds...";
-        
-        setTimeout(function() {
-            document.getElementById("wait").innerText="The download will start in 3 seconds...";
-            
-            setTimeout(function() {
-                document.getElementById("wait").innerText="The download will start in 2 seconds...";
-                
-                setTimeout(function() {
-                    document.getElementById("wait").innerText="The download will start in 1 seconds...";
-                    
-                    setTimeout(function() {
-                        document.getElementById("wait").innerText="Downloading..";
-                        stat(FileName, link);
-                    }, timelift);
-    
-                }, timelift);
-    
-            }, timelift);
-    
-        }, timelift);
-    
-    }, timelift);
- }
-});
+    if(downloading == false){
+        downloading = true;
+        timer();
+    }
+}
+
+function timer(){
+    if(delayMillis > 0){
+        document.getElementById("wait").innerText="The download will start in "+delayMillis+" seconds...";
+        delayMillis --;
+        setTimeout(timer(),1000);
+    }else{
+        document.getElementById("wait").innerText="Start Downloading..";
+        stat(FileName, link);
+    }
 }
